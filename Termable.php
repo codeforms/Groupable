2cb0b3a2fb30fcaeb21f83b7e1de95e93f6a7cac
codeforms <fatih.kececi@yahoo.co.uk>
2020-03-07 20:34:50 +0300

    syncTerm metot adı değiştirildi

--- Termable.php
+++ Termable.php
@@ -13,11 +13,11 @@ trait Termable
      * seçilen terimler arasında bağlantı oluşturma
      * 
      * @param array $terms : terim id'leri
-     * @example $post->syncTerms([1, 2, 3])
+     * @example $post->addTerms([1, 2, 3])
      * 
      * @return bool
      */
-    public function syncTerms(array $terms)
+    public function addTerms(array $terms)
     {
         return $this->termable()->sync($terms);
     }
